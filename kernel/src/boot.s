4efa0e9242487e1b5b1a3583b9583b777098e022
Noam DA SILVA <dasilvanoam@savio-lambersart.com>
2026-04-17 08:14:26 +0200

    Update boot.s

--- kernel/src/boot.s
+++ kernel/src/boot.s
@@ -1,3 +1,16 @@
+; Header Multiboot pour le mode graphique
+section .multiboot
+    align 4
+    dd 0x1BADB002             ; Magic
+    dd 0x05                   ; Flags (Graphics + Modules)
+    dd -(0x1BADB002 + 0x05)   ; Checksum
+    
+    ; Paramètres graphiques
+    dd 0, 0, 0, 0, 0
+    dd 0                      ; 0 = Mode graphique
+    dd 1024                   ; Largeur
+    dd 768                    ; Hauteur
+    dd 32                     ; Couleurs (32 bits)
 ; Bootloader Multiboot
 section .multiboot
     dd 0x1BADB002        ; Magic number
